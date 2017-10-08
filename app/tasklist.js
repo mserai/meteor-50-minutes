382f57d8b0e3c1ae326ea8ad731b03f34f645288
mserai <mserai@hawaii.edu>
2017-10-07 22:12:16 -1000

    Tasklist 4: Tasklist authentication

--- app/tasklist.js
+++ app/tasklist.js
@@ -2,25 +2,22 @@ Tasks = new Mongo.Collection('tasks');
 
 if (Meteor.isClient) {
   Template.tasks.helpers({
-    tasks: function () {
-      return Tasks.find({}, { sort: { createdAt: -1 } });
+    tasks: function() {
+      return Tasks.find({}, {sort: {createdAt: -1}});
     }
   });
 
   Template.tasks.events({
-    "submit .add-task": function (event) {
+    "submit .add-task": function(event) {
       var name = event.target.name.value;
-      Tasks.insert({
-        name: name,
-        createdAt: new Date()
-      });
+      Meteor.call('addTask', name);
       event.target.name.value = '';
       return false;
     },
 
-    "click .delete-task": function (event) {
+    "click .delete-task": function(event) {
       if (confirm('Delete Task?')) {
-        Tasks.remove(this._id);
+        Meteor.call('deleteTask', this._id);
       }
       return false;
     }
@@ -30,3 +27,18 @@ if (Meteor.isClient) {
 if (Meteor.isServer) {
 
 }
+
+Meteor.methods({
+  addTask: function(name){
+    if (!Meteor.userId()) {
+      throw new Meteor.Error('No Access');
+    }
+    Tasks.insert({
+      name: name, createdAt: new Date(), userId: Meteor.userId()
+    });
+  },
+
+  deleteTask: function(taskId) {
+    Tasks.remove(taskId);
+  }
+})
